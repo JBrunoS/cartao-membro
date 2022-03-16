b815f33c983bc7c4f91dc5fd7e367e37f534efe8
João Bruno Sousa <j_bruno_s@hotmail.com>
2022-03-16 09:47:57 -0300

    altera tabela users e seus controllers

---
+++ src/database/migrations/20220316124725_alter_table_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.alterTable('users', function(table){
+        table.boolean('status');
+    })
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.alterTable('users', function(table){
+          table.dropColumn('status');
+      })
+  };
+  
